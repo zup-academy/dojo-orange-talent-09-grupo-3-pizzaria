d2f3d75cdbbe3aad372494c6f66ef1caa8020cc0
marciogamazup <marcio.gama@zup.com.br>
2021-10-29 10:27:00 -0300

    Marcio Piloto Adrian Copiloto

--- src/test/java/br/com/zup/edu/pizzaria/ingredientes/cadastrodeingredientes/NovoIngredienteControllerTest.java
+++ src/test/java/br/com/zup/edu/pizzaria/ingredientes/cadastrodeingredientes/NovoIngredienteControllerTest.java
@@ -43,7 +43,22 @@ class NovoIngredienteControllerTest {
         mvc.perform(request)
            .andExpect(status().isCreated())
            .andExpect(header().exists("Location"))
-                .andExpect(redirectedUrlPattern("/api/ingredientes/\\d"));
+                .andExpect(redirectedUrlPattern("/api/ingredientes/*"));
+
+    }
+
+    @Test
+    void deveCadastrarNovoIngrediente() throws Exception {
+
+        NovoIngredienteRequest body = new NovoIngredienteRequest("Queijo muçarela", new BigDecimal("2.0"), 200);
+        MockHttpServletRequestBuilder request = post("/api/ingredientes")
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .content(new ObjectMapper().writeValueAsString(body));
+
+        mvc.perform(request)
+           .andExpect(status().isCreated())
+           .andExpect(header().exists("Location"))
+                .andExpect(redirectedUrlPattern("/api/ingredientes/*"));
 
     }
 }
