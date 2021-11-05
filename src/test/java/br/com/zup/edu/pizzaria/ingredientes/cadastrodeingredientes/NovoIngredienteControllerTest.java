a11ccd10db50457fee1a3e718d0610faa05a788a
marciogamazup <marcio.gama@zup.com.br>
2021-11-05 10:01:11 -0300

    Marcio - Todos

--- src/test/java/br/com/zup/edu/pizzaria/ingredientes/cadastrodeingredientes/NovoIngredienteControllerTest.java
+++ src/test/java/br/com/zup/edu/pizzaria/ingredientes/cadastrodeingredientes/NovoIngredienteControllerTest.java
@@ -2,29 +2,18 @@ package br.com.zup.edu.pizzaria.ingredientes.cadastrodeingredientes;
 
 import br.com.zup.edu.pizzaria.ingredientes.Ingrediente;
 import br.com.zup.edu.pizzaria.ingredientes.IngredienteRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import javax.transaction.Transactional;
-
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
---
+++ src/test/java/br/com/zup/edu/pizzaria/pizzas/PizzaTest.java
@@ -0,0 +1,21 @@
+package br.com.zup.edu.pizzaria.pizzas;
+
+import br.com.zup.edu.pizzaria.ingredientes.Ingrediente;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PizzaTest {
+
+    private Pizza pizza;
+
+    @Test
+    public void deveCalcularPrecoDaPizza() {
+        Ingrediente ingrediente1 = new Ingrediente("Muçarela", 10, new BigDecimal("15.0"));
+        Pizza pizza1 = new Pizza("Calabresa",)
+
+    }
+
+}
